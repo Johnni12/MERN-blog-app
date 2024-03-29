4722b81bba076e395b6794ac828dfc2df10c0567
kalu john <kalu.johnagu@gmail.com>
2024-03-29 15:28:16 +0100

    Header component update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,51 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../images/logo.png'
-import {FaBars} from 'react-icons/fa'
-import {AiOutlineClose} from 'react-icons/ai'
+import { FaBars } from 'react-icons/fa'
+import { AiOutlineClose } from 'react-icons/ai'
 
 const Header = () => {
- const [isOpen, setIsOpen] = useState()
+  const [isOpen, setIsOpen] = useState(window.innerWidth > 800 ? true : false)
+  const toggleNavbar = () => {
+    if (window.innerWidth < 800) {
+      setIsOpen(false)
+    } else {
+      setIsOpen(true)
+    }
+  }
   return (
-    
     <nav>
       <div className='container nav__container'>
-        <Link to='/' className='nav__logo'>
+        <Link to='/' className='nav__logo' onClick={toggleNavbar}>
           <img src={Logo} alt='Navbar Logo' />
         </Link>
-        <ul className='nav__menu'>
-          <li>
-            <Link to='/profile/:id'>Kalu John</Link>
-          </li>
-          <li>
-            <Link to='/create'>Create Post</Link>
-          </li>
-          <li>
-            <Link to='/authors'>Authors</Link>
-          </li>
-          <li>
-            <Link to='/logout'>Logout</Link>
-          </li>
-        </ul>
-        <button className='nav__toggle-btn'>
-          <AiOutlineClose/>
+        {isOpen && (
+          <ul className='nav__menu'>
+            <li>
+              <Link to='/profile/:id' onClick={toggleNavbar}>
+                Kalu John
+              </Link>
+            </li>
+            <li>
+              <Link to='/create' onClick={toggleNavbar}>
+                Create Post
+              </Link>
+            </li>
+            <li>
+              <Link to='/authors' onClick={toggleNavbar}>
+                Authors
+              </Link>
+            </li>
+            <li>
+              <Link to='/logout' onClick={toggleNavbar}>
+                Logout
+              </Link>
+            </li>
+          </ul>
+        )}
+
+        <button className='nav__toggle-btn' onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? <AiOutlineClose /> : <FaBars />}
         </button>
       </div>
     </nav>
